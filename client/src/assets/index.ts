350f16c009c3cdcc741dd2dd41b9ddbf670df260
viniciusgmacedo <macedovinicius2004@gmail.com>
2025-05-20 19:32:06 -0300

    feat: componentized pet card

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,23 @@
 import LogoCITi from "./Logo.png";
-
 export { LogoCITi };
+
+import Alarm from "./alarm.svg";
+export { Alarm };
+
+import cat1 from "./cat1.svg";
+export { cat1 };
+
+import cat3 from "./cat3.svg";
+export { cat3 };
+
+import cat4 from "./cat4.svg";
+export { cat4 };
+
+import cat5 from "./cat5.svg";
+export { cat5 };
+
+import cat6 from "./cat6.svg";
+export { cat6 };
+
+import cat2 from "./cat2.svg";
+export { cat2 };
---
+++ client/src/components/ui/pet-card.tsx
@@ -0,0 +1,190 @@
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+import { Alarm } from "@/assets";
+import { cat1 } from "@/assets";
+import { cat2 } from "@/assets";
+import { cat3 } from "@/assets";
+import { cat4 } from "@/assets";
+import { cat5 } from "@/assets";
+import { cat6 } from "@/assets";
+import { Button } from "./button";
+
+interface PetCardProps {
+  type: "Primeira consulta" | "Vacinacao" | "Checkup" | "Retorno" | "Historico";
+  nomepet: string;
+  nomedono: string;
+  nomedr: string;
+  data: string;
+  horario: string;
+  imagem: StaticImageData;
+}
+
+export default function PetCard(props: PetCardProps) {
+  switch (props.type) {
+    case "Primeira consulta":
+      return (
+        <div className="w-[495px] h-[135px] bg-primeiraconsulta rounded-2xl flex items-center">
+          <div className="w-[51px] h-[90px] bg-white rounded-sm ml-[24px] flex-col">
+            <Image src={Alarm} alt="alarme" className="ml-[17px] mt-[12px]" />
+
+            <div className="font-sf font-bold mt-[6px] ml-[5px]">
+              {props.data}
+            </div>
+
+            <div className="font-sf font-bold ml-[5px]">{props.horario}</div>
+          </div>
+
+          <div className="ml-[20px] font-bold font-sf">{props.nomedono}</div>
+
+          <div className="ml-[0px] font-sf">/{props.nomepet}</div>
+
+          <div className="ml-[20px] font-sf">{props.nomedr}</div>
+
+          <div className="flex-col flex justify-center items-center ml-auto mr-[20px]">
+            <Image
+              src={props.imagem}
+              alt="gato"
+              className="w-[70px] h-[70px] mb-[5px]"
+            />
+
+            <div className="w-[101px] h-[25px] bg-white rounded-sm font-sf text-[12px] flex justify-center items-center">
+              <div className="">{props.type}</div>
+            </div>
+          </div>
+        </div>
+      );
+
+    case "Vacinacao":
+      return (
+        <div className="w-[495px] h-[135px] bg-vacinacao rounded-2xl flex items-center">
+          <div className="w-[51px] h-[90px] bg-white rounded-sm ml-[24px] flex-col">
+            <Image src={Alarm} alt="alarme" className="ml-[17px] mt-[12px]" />
+
+            <div className="font-sf font-bold mt-[6px] ml-[5px]">
+              {props.data}
+            </div>
+
+            <div className="font-sf font-bold ml-[5px]">{props.horario}</div>
+          </div>
+
+          <div className="ml-[20px] font-bold font-sf">{props.nomedono}</div>
+
+          <div className="ml-[0px] font-sf">/{props.nomepet}</div>
+
+          <div className="ml-[20px] font-sf">{props.nomedr}</div>
+
+          <div className="flex-col flex justify-center items-center ml-auto mr-[20px]">
+            <Image
+              src={props.imagem}
+              alt="gato"
+              className="w-[70px] h-[70px] mb-[5px]"
+            />
+
+            <div className="w-[101px] h-[25px] bg-white rounded-sm font-sf text-[12px] flex justify-center items-center">
+              <div className="">{props.type}</div>
+            </div>
+          </div>
+        </div>
+      );
+
+    case "Checkup":
+      return (
+        <div className="w-[495px] h-[135px] bg-checkup rounded-2xl flex items-center">
+          <div className="w-[51px] h-[90px] bg-white rounded-sm ml-[24px] flex-col">
+            <Image src={Alarm} alt="alarme" className="ml-[17px] mt-[12px]" />
+
+            <div className="font-sf font-bold mt-[6px] ml-[5px]">
+              {props.data}
+            </div>
+
+            <div className="font-sf font-bold ml-[5px]">{props.horario}</div>
+          </div>
+
+          <div className="ml-[20px] font-bold font-sf">{props.nomedono}</div>
+
+          <div className="ml-[0px] font-sf">/{props.nomepet}</div>
+
+          <div className="ml-[20px] font-sf">{props.nomedr}</div>
+
+          <div className="flex-col flex justify-center items-center ml-auto mr-[20px]">
+            <Image
+              src={props.imagem}
+              alt="gato"
+              className="w-[70px] h-[70px] mb-[5px]"
+            />
+
+            <div className="w-[101px] h-[25px] bg-white rounded-sm font-sf text-[12px] flex justify-center items-center">
+              <div className="">{props.type}</div>
+            </div>
+          </div>
+        </div>
+      );
+
+    case "Retorno":
+      return (
+        <div className="w-[495px] h-[135px] bg-retorno rounded-2xl flex items-center">
+          <div className="w-[51px] h-[90px] bg-white rounded-sm ml-[24px] flex-col">
+            <Image src={Alarm} alt="alarme" className="ml-[17px] mt-[12px]" />
+
+            <div className="font-sf font-bold mt-[6px] ml-[5px]">
+              {props.data}
+            </div>
+
+            <div className="font-sf font-bold ml-[5px]">{props.horario}</div>
+          </div>
+
+          <div className="ml-[20px] font-bold font-sf">{props.nomedono}</div>
+
+          <div className="ml-[0px] font-sf">/{props.nomepet}</div>
+
+          <div className="ml-[20px] font-sf">{props.nomedr}</div>
+
+          <div className="flex-col flex justify-center items-center ml-auto mr-[20px]">
+            <Image
+              src={props.imagem}
+              alt="gato"
+              className="w-[70px] h-[70px] mb-[5px]"
+            />
+
+            <div className="w-[101px] h-[25px] bg-white rounded-sm font-sf text-[12px] flex justify-center items-center">
+              <div className="">{props.type}</div>
+            </div>
+          </div>
+        </div>
+      );
+
+    case "Historico":
+      return (
+        <div className="w-[495px] h-[135px] bg-historico rounded-2xl flex items-center">
+          <div className="w-[51px] h-[90px] bg-white rounded-sm ml-[24px] flex-col">
+            <Image src={Alarm} alt="alarme" className="ml-[17px] mt-[12px]" />
+
+            <div className="font-sf font-bold mt-[6px] ml-[5px]">
+              {props.data}
+            </div>
+
+            <div className="font-sf font-bold ml-[5px]">{props.horario}</div>
+          </div>
+
+          <div className="ml-[20px] font-bold font-sf">{props.nomedono}</div>
+
+          <div className="ml-[0px] font-sf">/{props.nomepet}</div>
+
+          <div className="ml-[20px] font-sf">{props.nomedr}</div>
+
+          <div className="flex-col flex justify-center items-center ml-auto mr-[20px]">
+            <Image
+              src={props.imagem}
+              alt="gato"
+              className="w-[70px] h-[70px] mb-[5px]"
+            />
+
+            <div className="w-[101px] h-[25px] bg-white rounded-sm font-sf text-[12px] flex justify-center items-center">
+              <div className="">{props.type}</div>
+            </div>
+          </div>
+        </div>
+      );
+  }
+}
