99e2d0a8ad3b0dddeb8b32aaa86ad2faca02ed05
Vitor <jvab2@cin.ufpe.br>
2025-05-28 12:15:44 -0300

    tela de cadastro completa, erro na foto do cachorro

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,9 +2,16 @@ import Buttongroup from "./Button groups.svg"
 import LogoCITi from "./Logo.png";
 import Logopet from "./Logo CITi Pet.svg"
 import SetaVoltar from"./SetaVoltar.svg"
+import boi from"./boi.svg"
+import cavalo from "./cavalo.svg"
+import cachorro from"./cachorro.svg"
+import gato from"./gato.svg"
+import ovelha from"./ovelha.svg"
+import porco from"./porco.svg"
+import calendario from "./calendario.svg"
+import relogio from "./relogio.svg"
 
-
-
+export {boi,cavalo,cachorro,gato,ovelha,porco,calendario,relogio}
 export {SetaVoltar};
 export { LogoCITi };
 export{Logopet};
